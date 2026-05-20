d269c95721fe279adc099841514055726e240843
samsanchez-ctrl <sam.sanchez@duocuc.cl>
2026-05-20 12:46:07 -0400

    arreglando pipeline frontend, app.js y default.config

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // Determinar la URL base de la API según el host
 // frontend/app.js
 
-const API_BASE = "http://18.209.107.114:3001/api/productos";
+const API_BASE = "http://10.0.137.129:3001/api/productos";
 
 // Ejemplo: const API_BASE = "http://10.0.2.30:3001/api/productos";
 
